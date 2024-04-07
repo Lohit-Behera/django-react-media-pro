e2381fc8957cefa4c08039ba529b70a87837bc06
Lohit-Zeno <lohitsekhar.2002@gmail.com>
2024-04-07 07:46:21 +0530

    remove script

--- scripts/gunicorn.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-PROJECT_MAIN_DIR_NAME="django-react-media-pro"
-
-sudo cp "/home/ubuntu/$PROJECT_MAIN_DIR_NAME/gunicorn/gunicorn.socket" "/etc/systemd/system/gunicorn.socket"
-sudo cp "/home/ubuntu/$PROJECT_MAIN_DIR_NAME/gunicorn/gunicorn.service" "/etc/systemd/system/gunicorn.service"
-
-sudo systemctl start gunicorn.service
-sudo systemctl enable gunicorn.service
